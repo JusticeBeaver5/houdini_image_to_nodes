98b8117c2b79d6660e346414aa3f301102978018
JusticeBeaver5 <ruslanshaov@gmail.com>
2023-04-22 22:22:57 +0300

    working

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from PIL import Image
-# import hou
+import hou
 import time
 
 start = time.perf_counter()
 dir = 'D:\\python\\image_processing\\'
 
-# obj = hou.node('/obj')
+obj = hou.node('/obj')
 
 with Image.open(f'{dir}thats_what_she_said3.jpg') as img:
     px = img.load()
@@ -23,9 +23,9 @@ for i in range(width):
         # print(i, j)
         px_color = img.getpixel((i, j))
         print(px_color)
-        # null = obj.createNode('null',f'null_{i}_{j}')
-        # null.setPosition([i*1.5, j/2])
-        # null.setColor(hou.Color(px_color[0]/255, px_color[1]/255, px_color[2]/255))
+        null = obj.createNode('null',f'null_{i}_{j}')
+        null.setPosition([-i*1.5, -j/2])
+        null.setColor(hou.Color(px_color[0]/255, px_color[1]/255, px_color[2]/255))
 
 
 print(f'execution time = {time.perf_counter() - start} ')
