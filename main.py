553b2e5b2219460a9addd3ffb6d974dc92069f92
JusticeBeaver5 <ruslanshaov@gmail.com>
2023-05-19 17:42:41 +0300

    using threading

--- main.py
+++ main.py
@@ -1,31 +1,30 @@
 from PIL import Image
 import hou
 import time
+from threading import Thread
 
-start = time.perf_counter()
 dir = 'D:\\python\\image_processing\\'
 
 obj = hou.node('/obj')
 
-with Image.open(f'{dir}thats_what_she_said3.jpg') as img:
+with Image.open(f'{dir}thats_what_she_said4.jpg') as img:
     px = img.load()
 
 width, height = img.size
 
 print(width, height)
-# print(img.getpixel((1, 10)))
-# px_color = img.getpixel((68, 59))
 
 
+def createMosaic(img, width, height):
+    start = time.perf_counter()
+    for i in range(height):
+        for j in range(width):
+            px_color = img.getpixel((j, i))
+            null = obj.createNode('null',f'_{i}_{j}')
+            null.setPosition([j*0.75, -i/2])
+            null.setColor(hou.Color(px_color[0]/255, px_color[1]/255, px_color[2]/255))
+    print(f'execution time = {time.perf_counter() - start} ')
 
-for i in range(width):
-    for j in range(height):
-        # print(i, j)
-        px_color = img.getpixel((i, j))
-        print(px_color)
-        null = obj.createNode('null',f'_{i}_{j}')
-        null.setPosition([i*0.75, -j/2])
-        null.setColor(hou.Color(px_color[0]/255, px_color[1]/255, px_color[2]/255))
-
-
-print(f'execution time = {time.perf_counter() - start} ')
+    
+th = Thread(target=createMosaic, args=[img, width, height], daemon=True)
+th.start()
